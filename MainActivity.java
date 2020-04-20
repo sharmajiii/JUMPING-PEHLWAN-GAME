9112fa50dea9887e826bb5a1bdcea18ebfc44aba
sharmaji <49364582+sharmajiii@users.noreply.github.com>
2020-04-21 00:24:48 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.sharmaji.game;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent;
+                        intent=new Intent(getApplicationContext(),AndroidLauncher.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
